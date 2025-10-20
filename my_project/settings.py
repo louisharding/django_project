1d83dacda0672c7696b48180fdb715bdc7a2ca87
louisharding <lgharding0@gmail.com>
2025-10-20 15:57:42 +0100

    Add Procfile, update settings for production, and modify requirements

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)#q*qqqi))4nceo&bio_xan)z!u)08zknil5-rc)$1hxvvxmxw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
